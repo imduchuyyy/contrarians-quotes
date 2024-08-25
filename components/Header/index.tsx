16908208c8d11c3c1c2002ccdc248facb3dad942
misterbo271 <nevermind271999@gmail.com>
2024-08-25 12:01:46 +0700

    fix responsive

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -25,10 +25,10 @@ const Header = ({ title, visible, visibleBackButton, showMenu, onClickBurger }: 
     return (
         <>
             <div
-                className={`fixed top-0 right-0 z-10 bg-theme-n-8 md:z-30 md:transition-colors left-0 sm:left-10 md:left-5 ${showMenu ? "md:!bg-theme-on-surface-1" : ""}`}
+                className={`fixed top-0 md:top-4 right-0 z-10 bg-theme-n-8 md:z-30 md:transition-colors left-0 sm:left-3 md:left-5 ${showMenu ? "md:!bg-theme-on-surface-1" : ""}`}
             >
                 <div
-                    className={`relative flex items-center justify-between h-24 w-full mx-auto px-10 lg:px-6 md:h-16 md:px-4 md:rounded-2xl md:transition-shadow ${
+                    className={`relative flex items-center justify-between h-24 w-full mx-auto px-10 sm:px-0 lg:px-6 md:h-16 md:px-4 md:rounded-2xl md:transition-shadow ${
                         showMenu
                             ? "shadow-depth-1 dark:shadow-[inset_0_0_0_0.125rem_#272B30]"
                             : ""
@@ -36,7 +36,7 @@ const Header = ({ title, visible, visibleBackButton, showMenu, onClickBurger }: 
                 >
                     {visibleBackButton ? (
                         <button
-                            className="group inline-flex items-center mr-auto text-xl md:hidden"
+                            className="group inline-flex items-center mr-auto"
                             onClick={() => router.back()}
                         >
                             <div className="flex justify-center items-center w-6 h-6 mr-3.5 lg:mr-1">
@@ -45,16 +45,16 @@ const Header = ({ title, visible, visibleBackButton, showMenu, onClickBurger }: 
                                     name="arrow-left"
                                 />
                             </div>
-                            Try another one
+                            <div className="md:hidden text-xl">Try another one</div>
                         </button>
                     ) : null}
 
-                    <div className="absolute left-1/2 transform -translate-x-1/2 text-h3 md:text-h4 sm:text-h5 text-center font-bold">
+                    <div className="absolute left-1/2 transform -translate-x-1/2 text-h3 sm:text-xl md:text-2xl md:text-h4 sm:text-h5 text-center font-bold">
                         Your Contrarian types
                     </div>
 
                     <button
-                        className="absolute right-10 group flex items-center h-12 px-4 rounded-xl transition-colors hover:bg-theme-on-surface-2 md:hover:bg-transparent lg:right-6"
+                        className="absolute right-10 group flex items-center h-12 px-4 sm:px-0 rounded-xl transition-colors hover:bg-theme-on-surface-2 md:hover:bg-transparent lg:right-6"
                         onClick={toggleColorMode}
                     >
                         <Icon
