cc1f7e2cb21171f68f9e1747edb5c2502a0cd51a
imduchuyyy <duchuy.124dk@gmail.com>
2024-08-26 12:01:03 +0700

    chore: Refactor SponsorInfo component layout for better responsiveness

--- components/SponsorInfo/index.tsx
+++ components/SponsorInfo/index.tsx
@@ -5,34 +5,42 @@ const SponsorInformation = () => {
   const isLightMode = colorMode === "light";
 
   return (
-    <div>
-      <div className="fixed bottom-6 text-base-1s left-1/2 transform -translate-x-1/2 mb-4 sm:w-full text-center">
-        Built By{" "}
-        <a
-          href="https://x.com/imduchuyyy"
-          target="_blank"
-          className={`${isLightMode ? "text-red-600" : "text-red-200"} italic`}
-        >
-          @imduchuyyy
-        </a>
-      </div>
-      <div className="fixed bottom-0 text-base-1s left-1/2 transform -translate-x-1/2 mb-4 sm:w-full text-center">
-        By me a coffee:{" "}
-        <a
-          href="https://etherscan.io/address/imduchuyyy.eth"
-          target="_blank"
-          className={`${isLightMode ? "text-red-600" : "text-red-200"} italic`}
-        >
-          imduchuyyy.eth
-        </a>
-        {" | "}
-        <a
-          href="https://vicscan.xyz/address/imduchuyyy.c98"
-          target="_blank"
-          className={`${isLightMode ? "text-red-600" : "text-red-200"} italic`}
-        >
-          imduchuyyy.c98
-        </a>
+    <div className="mt-4 fixed sm:static w-full bottom-0 left-0">
+      <div>
+        <div className="text-base-1s sm:w-full text-center">
+          Built By{" "}
+          <a
+            href="https://x.com/imduchuyyy"
+            target="_blank"
+            className={`${
+              isLightMode ? "text-red-600" : "text-red-200"
+            } italic`}
+          >
+            @imduchuyyy
+          </a>
+        </div>
+        <div className="text-base-1s mb-4 sm:w-full text-center">
+          By me a coffee:{" "}
+          <a
+            href="https://etherscan.io/address/imduchuyyy.eth"
+            target="_blank"
+            className={`${
+              isLightMode ? "text-red-600" : "text-red-200"
+            } italic`}
+          >
+            imduchuyyy.eth
+          </a>
+          {" | "}
+          <a
+            href="https://vicscan.xyz/address/imduchuyyy.c98"
+            target="_blank"
+            className={`${
+              isLightMode ? "text-red-600" : "text-red-200"
+            } italic`}
+          >
+            imduchuyyy.c98
+          </a>
+        </div>
       </div>
     </div>
   );
